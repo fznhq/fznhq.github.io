177e589aa8015a2cafd74953c15a85e8a0878621
Fauzan Haq <55099255+fznhq@users.noreply.github.com>
2023-03-05 12:51:27 +0700

    Updates

--- _app/immutable/chunks/1.cc8adab1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.0363e9b6.js";export{t as component};
--- _app/immutable/chunks/2.38cae3f3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.3f2cf1fe.js";export{t as component};
